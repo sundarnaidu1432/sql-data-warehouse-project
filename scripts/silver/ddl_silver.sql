345e0d4b88f515332c27e5ca975725fac5d1793d
sundarnaidu1432 <kandrahemasundarnaidu1432@gmail.com>
2025-09-24 19:18:03 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,6 +1,11 @@
-IF OBJECT_ID('silver.crm_cust_info','U') IS NOT NULL
-DROP TABLE silver.crm_cust_info;
-
+/*
+===================================================================================
+DDL Script: Create Silver Tables
+===================================================================================
+Script Purpose:
+	This script creates tables in the 'silver' schema, dropping existing tables if they already exist.
+Run this script to re-define the DDL structure of 'bronze Tables
+*/
 GO
   
 CREATE TABLE silver.crm_cust_info(
@@ -14,19 +19,15 @@ CREATE TABLE silver.crm_cust_info(
 	dwh_create_date    DATETIME2 DEFAULT GETDATE()
 );
 
-IF OBJECT_ID('silver.crm_prd_info','U') IS NOT NULL
-DROP TABLE silver.crm_prd_info;
-
-GO
-  
 CREATE TABLE silver.crm_prd_info(
  	prd_id			INT,
+	cat_id			NVARCHAR(50),
  	prd_key			NVARCHAR(50),
  	prd_nm			NVARCHAR(50),
  	prd_cost		INT,
  	prd_line		NVARCHAR(50),
- 	prd_start_dt	DATETIME,
- 	prd_end_dt		DATETIME,
+ 	prd_start_dt	DATE,
+ 	prd_end_dt		DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
@@ -39,9 +40,9 @@ CREATE TABLE silver.crm_sales_details(
  	sls_ord_num		NVARCHAR(50),
  	sls_prd_key		NVARCHAR(50),
  	sls_cust_id		INT,
- 	sls_order_dt	INT,
- 	sls_ship_dt		INT,
- 	sls_due_dt		INT,
+ 	sls_order_dt	DATE,
+ 	sls_ship_dt		DATE,
+ 	sls_due_dt		DATE,
  	sls_sales		INT,
  	sls_quantity	INT,
  	sls_price		INT,
